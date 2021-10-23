48c78542b677a56e3f3246ed9ed8d779a5f702d3
grisildarapanj <grisildarapanj@gmail.com>
2021-10-23 19:37:36 +0200

    Added to git

---
+++ detyr_C_2/src/ushtrim2C/WebCrawlerMain.java
@@ -0,0 +1,9 @@
+package ushtrim2C;
+import java.util.Scanner;
+public class WebCrawlerMain {
+	public static void main(String[] args) {
+	
+	    System.out.print("Hello");
+	     // Traverse the Web from the a starting url
+	  }
+}
